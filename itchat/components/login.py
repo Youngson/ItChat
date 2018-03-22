f138a4258306a0fb20b93bde3242ff60bb2b0f3d
没眉毛的青蛙 <youngson.gu@gmail.com>
2018-03-22 16:27:41 +0800

    * 取消core.loginInfo['logintime']注释
    Signed-off-by: Youngson <Youngson.gu@gmail.com>

--- itchat/components/login.py
+++ itchat/components/login.py
@@ -181,7 +181,7 @@ def process_login_info(core, loginContent):
     else:
         core.loginInfo['fileUrl'] = core.loginInfo['syncUrl'] = core.loginInfo['url']
     core.loginInfo['deviceid'] = 'e' + repr(random.random())[2:17]
-    # core.loginInfo['logintime'] = int(time.time() * 1e3)
+    core.loginInfo['logintime'] = int(time.time() * 1e3)
     core.loginInfo['BaseRequest'] = {}
     for node in xml.dom.minidom.parseString(r.text).documentElement.childNodes:
         if node.nodeName == 'skey':
